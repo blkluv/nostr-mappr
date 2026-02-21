5c958879e8ca931eb11f4729c49671fed8f6e79e
WEB5D <info@blkluv.org>
2026-02-20 19:27:03 -0500

    Update search-controller.js

--- src/features/search/search-controller.js
+++ src/features/search/search-controller.js
@@ -2,6 +2,7 @@ import { AuthManager } from '../../core/auth.js';
 import { CATEGORIES } from '../../core/categories.js';
 
 /* --- FLOATING UI ELEMENTS --- */
+export function initSearch(mapManager) {
 const userNameMini = document.getElementById('user-name-mini');
 const userAvatarMini = document.getElementById('user-avatar-small');
 const userPubkeyMini = document.getElementById('user-pubkey-mini');
